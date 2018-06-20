076e0045f2d22e58e2ca7fdf27166a0bae55c949
Ann Kim <annkim822@gmail.com>
2018-06-20 13:23:02 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  numbers = [1, 2, 3, 4]
-  numbers.push(5)
-  puts numbers
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
